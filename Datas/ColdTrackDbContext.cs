fb9c9355eebbaf5e3649c05bc9327ab56d9cbfd0
ABprogramming <WFB15376253289@outlook.com>
2025-08-06 04:48:04 +0800

    modify: DB初始化操作默认生成管理员

--- Datas/ColdTrackDbContext.cs
+++ Datas/ColdTrackDbContext.cs
@@ -15,26 +15,55 @@ public class ColdTrackDbContext(DbContextOptions<ColdTrackDbContext> options) : 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        
-        builder.Entity<AppUser>(entity =>
-        {
-            entity.Property(u => u.CreatedAt)
-                .HasColumnType("datetime2")
-                .HasDefaultValueSql("GETDATE()");
-        });
+        var roleAdminId = Guid.NewGuid().ToString();
+        var roleUserId = Guid.NewGuid().ToString();
+        var adminId = Guid.NewGuid().ToString();
+        // 生成角色
         var roles = new List<IdentityRole>
         {
             new()
             {
+                Id = roleAdminId,
                 Name = "Admin",
                 NormalizedName = "ADMIN"
             },
             new()
             {
+                Id = roleUserId,
                 Name = "User",
                 NormalizedName = "USER"
             }
         };
         builder.Entity<IdentityRole>().HasData(roles);
+        // 生成管理员
+        var admin = new AppUser
+        {
+            Id = adminId,
+            UserName = "admin@cold.com",
+            NormalizedUserName = "ADMIN@COLD.COM",
+            Email = "admin@cold.com",
+            NickName = "Admin",
+            City = "北京市",
+            NormalizedEmail = "ADMIN@COLD.COM",
+            EmailConfirmed = false,
+            PhoneNumber = "17323895436",
+            PhoneNumberConfirmed = false,
+            TwoFactorEnabled = false,
+            LockoutEnabled = true,
+            AccessFailedCount = 0,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            ConcurrencyStamp = Guid.NewGuid().ToString("D"),
+            CreatedAt = DateTime.UtcNow
+        };
+        var passwordHasher = new PasswordHasher<AppUser>();
+        admin.PasswordHash = passwordHasher.HashPassword(admin, "Admin123.");
+        builder.Entity<AppUser>().HasData(admin);
+        // 生成管理员的角色
+        var userRole = new IdentityUserRole<string>
+        {
+            RoleId = roleAdminId,
+            UserId = adminId
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRole);
     }
 }
